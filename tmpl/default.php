8d6771b714e84d3a860eb9d43bcc2085462001e0
Samundra Shrestha <samundra.shr@gmail.com>
2013-05-15 10:07:22 +0545

    template code cleanup

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,10 +8,15 @@ defined('_JEXEC') or die;
 <ul class="bxslider">
   <?php for($i=0;$i<20;$i++):?>
   <?php if(!empty($Image[$i])):?>
-  <li><h2><?php echo $Title[$i]; ?></h2>
+  <li>
+  	<div class="bx-slider-info">
+  	<h2><?php echo $Title[$i]; ?></h2>
           <p><?php echo $Text[$i]; ?></p>  
-        <?php if ($Link[$i] != null) {echo '<a href="'.$Link[$i].'" class="bx-link-readmore">'.JText::_('MOD_BX_SLIDER_READMORE_LINK').'</a>';}?>
-                <div class="da-img"><?php echo '<img src="'.$Image[$i].'"/>';?></div>
+           <?php if ($Link[$i] != null) {echo '<a href="'.$Link[$i].'" class="bx-link-readmore">'.JText::_('MOD_BX_SLIDER_READMORE_LINK').'</a>';}?>
+
+       </div>
+
+                <div class="bx-slider-image"><?php echo '<img src="'.$Image[$i].'"/>';?></div>
   </li>
   <?php endif;?>
   <?php endfor; ?>
